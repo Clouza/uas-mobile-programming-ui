1bfc14a0d0d7af6d07bb068965e3d214375e3691
Wahyu Siwananda <siwarjsa@gmail.com>
2025-07-05 10:10:14 +0800

    Back to local

--- app/src/main/java/com/example/uiclient/utils/RetrofitClient.java
+++ app/src/main/java/com/example/uiclient/utils/RetrofitClient.java
@@ -16,7 +16,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static String BASE_URL = "https://788c-2a09-bac5-d561-101e-00-19b-132.ngrok-free.app";
+    private static String BASE_URL = "http://10.0.2.2";
 
     public static Retrofit client() throws NoSuchAlgorithmException {
         OkHttpClient client = getUnsafeOkHttpClient();
